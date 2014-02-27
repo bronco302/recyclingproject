45d0b1ca19eb5bdb0f42f9452af48dffed31310a
Jonathan Ahumada <ahumada.jonathan@gmail.com>
2014-02-27 15:32:38 -0800

    Creation of ItemValidator, PaymentDispenser, RecyclableItem, and Scale. Getters and setters for RecyclableItem.

---
+++ RecyclingProject/src/rcm/RecyclableItem.java
@@ -0,0 +1,24 @@
+package rcm;
+
+public class RecyclableItem {
+	private String typeOfRecyclableItem;
+	private int weight;
+	
+	public RecyclableItem(){
+		typeOfRecyclableItem = new String("Not Specified");
+		weight = 0 ;
+	}
+	public RecyclableItem(String typeOfItem, int weight){
+		typeOfRecyclableItem = typeOfItem; 
+		this.weight = weight; 
+		
+	}
+	
+	
+	public String getTypeOfItem(){
+		return typeOfRecyclableItem; 
+	}
+	public int getWeight(){
+		return weight;
+	}
+}
