1562ef09bd4f5d558cd7c531b19667d37c05a3e9
stevenchua <steven.p.chua@gmail.com>
2014-02-28 18:01:24 -0800

    Update RecyclableItem.java

--- RecyclingProject/src/rcm/RecyclableItem.java
+++ RecyclingProject/src/rcm/RecyclableItem.java
@@ -9,18 +9,24 @@ package rcm;
 public class RecyclableItem {
 	private String typeOfRecyclableItem;
 	private int weight;
+	private double price; 
 	
 	public RecyclableItem(){
 		typeOfRecyclableItem = new String("Not Specified");
 		weight = 0 ;
 	}
-	public RecyclableItem(String typeOfItem, int weight){
+	public RecyclableItem(String typeOfItem, int weight, double price){
 		typeOfRecyclableItem = typeOfItem; 
 		this.weight = weight; 
+		this.price = price; 
+	
 		
 	}
 	
 	
+/**
+ * Given Constructors
+ */	
 	public String getTypeOfItem(){
 		return typeOfRecyclableItem; 
 	}
