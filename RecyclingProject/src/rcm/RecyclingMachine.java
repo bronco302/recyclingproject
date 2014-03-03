07c1f65d0af2d1cbcefcf8645d78301bc80d8610
stevenchua <steven.p.chua@gmail.com>
2014-03-02 20:03:50 -0800

    Create RecyclingMachine.java

---
+++ RecyclingProject/src/rcm/RecyclingMachine.java
@@ -0,0 +1,28 @@
+package rcm; 
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Observable;
+import java.util.Random;
+
+/**
+ * Performs the functions of a recycling machine.
+ * @author Jonathan & Steven 
+ *
+ */
+ 
+public class RecylingMachine extends Observable{
+  
+    public enum MachineHealth{
+      ALMOST_FULL, FULL; CASH_NEEDED; COUPONS_NEEDED; FUNCTIONING_WELL; 
+    }
+    public enum Aciivity{
+     INACTIVITY; ACTIVITY;
+    }
+    
+    private String machine_id; 
+    private String activity; 
+    private String machine_health; 
+    private double 
+    
+ 
